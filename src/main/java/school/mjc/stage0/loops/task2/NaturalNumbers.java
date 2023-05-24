3ab1465e9e23637da0fa9b3be18321f88625bd27
unoffending <109032978+unoffending@users.noreply.github.com>
2023-05-24 16:14:43 +0600

    Update NaturalNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task2;
 
 public class NaturalNumbers {
     public void naturalNumbersPrinter(int lastPrinted) {
+        int first = 1;
+        while(lastPrinted >= first) {
+            System.out.println(first);
+            first++;
+        }
     }
 }
